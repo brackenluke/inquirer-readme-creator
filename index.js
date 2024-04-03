05e0c80b17cf8dd41195cb2635386487bb8a0a50
brackenluke <156158313+brackenluke@users.noreply.github.com>
2024-04-02 23:24:54 -0500

    Added starter code. Started npm init and installed inquirer.

---
+++ index.js
@@ -0,0 +1,29 @@
+// // TODO: Include packages needed for this application
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+
+
+//Including packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+//Creating an array of questions for user input
+const questions = [
+ 
+
+];
+
+
